d47eed2bad505e78a5ee02b6e0a8e758694e842e
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-01-26 19:34:48 +0000

    Updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './styles/style.css';
 import axios from 'axios';
-import NEWS_API_KEY from './js/apikey';
 import Menu from './js/Menu';
 import ScrollTop from './js/ScrollTop';
 
@@ -8,28 +7,27 @@ const menu = new Menu();
 const scrollTop = new ScrollTop();
 
 const newsContainer = document.querySelector('.general-news__container');
-const apiKey = NEWS_API_KEY;
 let limit = 10;
 let category = ['home', 'sports', 'world', 'travel', 'health', 'food'];
-let url = `https://api.nytimes.com/svc/topstories/v2/${category[0]}.json?api-key=${apiKey}`;
+let url = `https://api.nytimes.com/svc/topstories/v2/${category[0]}.json?api-key=${process.env.API_KEY}`;
 
 const getArticles = async () => {
-	try {
-		const response = await axios({
-			url: url,
-			method: 'GET',
-		});
-		return response.data.results;
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    const response = await axios({
+      url: url,
+      method: 'GET'
+    });
+    return response.data.results;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const newsTemplate = (article) =>
-	`
+  `
   <article class="card">
     <a href=${article.url}>
-    <img src=${article.multimedia[3].url} class="card__img" alt="card__img" />
+    <img src=${article.multimedia[1].url} class="card__img" alt="card__img" />
     </a>
     <div>
       <div class="card__category">${article.section}</div>
@@ -46,44 +44,50 @@ const newsTemplate = (article) =>
   `;
 
 const htmlTemplate = async () => {
-	const data = await getArticles();
-	return newsContainer.insertAdjacentHTML(
-		'afterbegin',
-		`
+  const data = await getArticles();
+  return newsContainer.insertAdjacentHTML(
+    'afterbegin',
+    `
   ${data
-		.map((article) => newsTemplate(article))
-		.slice(0, 15)
-		.join('')}
+    .map((article) => newsTemplate(article))
+    .slice(0, 3)
+    .join('')}
  `
-	);
+  );
 };
 
 htmlTemplate();
 
 const getNewsCategory = () => {
-	const categoryLink = document.querySelectorAll('.menu__list-link');
-	categoryLink.forEach((link) => {
-		link.addEventListener('click', async () => {
-			document.querySelector(
-				'.news-title'
-			).innerHTML = `${link.innerHTML.toUpperCase()}`;
-			try {
-				const response = await axios({
-					url: `https://api.nytimes.com/svc/topstories/v2/${link.innerHTML.toLowerCase()}.json?api-key=${apiKey}`,
-					method: 'GET',
-				});
-				const data = response.data.results;
-				return newsContainer.insertAdjacentHTML(
-					'afterbegin',
-					`
-  ${data.map((article) => newsTemplate(article)).join('')}
+  const categoryLink = document.querySelectorAll('.menu__list-link');
+  categoryLink.forEach((link) => {
+    link.addEventListener('click', async () => {
+      document.querySelector(
+        '.news-title'
+      ).innerHTML = `${link.innerHTML.toUpperCase()}`;
+      try {
+        const response = await axios({
+          url: `https://api.nytimes.com/svc/topstories/v2/${link.innerHTML.toLowerCase()}.json?api-key=${
+            process.env.API_KEY
+          }`,
+          method: 'GET'
+        });
+        const data = response.data.results;
+
+        return newsContainer.insertAdjacentHTML(
+          'afterbegin',
+          `
+  ${data
+    .map((article) => newsTemplate(article))
+    .slice(0, 3)
+    .join('')}
  `
-				);
-			} catch (err) {
-				console.log(err);
-			}
-		});
-	});
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
 };
 
 getNewsCategory();
